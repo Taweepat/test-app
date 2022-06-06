1f57aab70937c30f269387aa2f6e65cf03ae9ea3
Taweepat <mu.taweepat_st@tni.ac.th>
2022-06-06 16:05:49 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Detail from './componant/Detail';
 import Header from './componant/Header';
+import './App.css';
 
 function App() {
   return (
--- src/componant/Detail.js
+++ src/componant/Detail.js
@@ -9,7 +9,8 @@ const detail = () => {
     useEffect(() => {
         axios.get(URL).then((res) => {
             setPost(res.data)
-            // console.log(res.data)
+            console.log(res.data)
+            // console.log(res.data.products.find(product => product.id == 9))
         })
     }, [])
     if (!post) return null;
@@ -17,21 +18,23 @@ const detail = () => {
         <div className='container'>
             <div className="boxs">
                 {
-                    post.products.map((n,index) => {
-                        return (
-                            <div className="block" key={n.id}>
-                            <h3>{n.title}</h3>
-                            <h2>{n.description}</h2>
-                            <p>เริ่มต้นที่ ฿{n.price}</p>
+                    post.products.filter((product)=>product.id == 9).map((product)=>{
+                        return(
+                            <div className="block" key={product.id}>
+                            <h3>{product.title}</h3>
+                            <h2>{product.description}</h2>
+                            <p>เริ่มต้นที่ ฿{product.price}</p>
                             <div>
                             <button className='button'>ซื้อ</button><span><a href='#'>ดูเพิ่มเติม &rsaquo;</a></span>
                             </div>
                             <div className="image">
-                            <img src={n.thumbnail}/>
+                            <img src={product.thumbnail}/>
                             </div>
                             </div>
                         )
                     })
+                        // if (n.id = 9)
+                        // // console.log("post.product",post.products)
                 }
             </div>
         </div>
